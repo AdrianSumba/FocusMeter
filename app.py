4ae72e689880cd2d0859842b714ac4f6350cb5fa
Christian Mendieta <christian.mendieta.t@gmail.com>
2026-01-04 10:21:20 -0500

    Agregando páginas de streamlit.

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Focus Meter Web",
+    layout="wide"
+)
+
+pg = st.navigation([
+    st.Page("paginas/home.py", title="🏠 Home"),
+    st.Page("paginas/semaforo.py", title="🚦 Semáforo"),
+    st.Page("paginas/estadisticas.py", title="📊 Estadísticas"),
+    st.Page("paginas/docs.py", title="📖 Documentación"),
+])
+
+pg.run()
